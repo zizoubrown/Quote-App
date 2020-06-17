a37752b8046470865d2849785f5a66207fa97ac7
Zizoubrown <zizumoha2015@gmail.com>
2020-06-17 16:23:20 +0300

    fix errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Qoutes } from './qoutes';
+import { Qoutes } from './quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
